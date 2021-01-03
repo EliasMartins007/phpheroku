15368ffbe3bcadd18dde7c51d7358ad678ecad52
elias <eliasfaculdade@yahoo.com.br>
2021-01-03 15:19:52 -0300

    heroku3

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-echo '<h2>Hospedando seu projeto PHP no Heroku</h2>';
+//echo '<h2>Hospedando seu projeto PHP no Heroku</h2>';
 
 //mysqli_connect("host", "user", "password", "db") or die(mysqli_error());
 
 //echo "Connected to MySQL<br />";
 
-phpinfo();
-
+//phpinfo();
+include_once("index.html");
 ?>
